d92e559137e7f4de224511804ef5186f248283f7
Ben Greenman <benjaminlgreenman@gmail.com>
2020-08-11 18:43:19 -0400

    blame: convert pidigits

---
+++ dissertation/QA/blame-correct/rp/pidigits/main.py
@@ -0,0 +1,55 @@
+from Timer import Timer
+
+"""Calculating (some of) the digits of pi.  This stresses big integer
+arithmetic."""
+
+"""
+bg
+- remove toplevel function, add `ITERATIONS` constant
+- replace `timer` with `Timer`
+- replaced iterators with lists
+- increased NDIGITS to `5000`
+"""
+
+#import itertools
+
+NDIGITS = 5000
+
+# Adapted from code on http://shootout.alioth.debian.org/
+def gen_x(k:int)->(int,int,int,int):
+    return (k, 4*k + 2, 0, 2*k + 1)
+
+def compose(a:(int,int,int,int), b:(int,int,int,int))->(int,int,int,int):
+    aq, ar, as_, at = a
+    bq, br, bs, bt = b
+    return (aq * bq,
+            aq * br + ar * bt,
+            as_ * bq + at * bs,
+            as_ * br + at * bt)
+
+def extract(z:(int,int,int,int), j:int)->int:
+    q, r, s, t = z
+    return (q*j + r) // (s*j + t)
+
+def pi_digits(limit:int)->List(int):
+    z = (1, 0, 0, 1)
+    x = 1
+    result = []
+    while (x <= limit):
+        y = extract(z, 3)
+        while y != extract(z, 4):
+            z = compose(z, gen_x(x))
+            y = extract(z, 3)
+        z = compose((10, -10*y, 0, 1), z)
+        x += 1
+        result.append(y)
+    return result
+
+def calc_ndigits(n:int)->List(int):
+    return pi_digits(n)
+
+if __name__ == "__main__":
+    t = Timer()
+    with t:
+        print(calc_ndigits(NDIGITS))
+
---
+++ dissertation/QA/blame-correct/tr/pidigits/main.rkt
@@ -0,0 +1,54 @@
+#lang typed/racket/base #:transient
+(require racket/match)
+
+(define NDIGITS : Natural 5000)
+
+(define-type IIII (List Integer Integer Integer Integer))
+
+(: gen_x (-> Integer IIII))
+(define (gen_x k)
+  (list k (+ (* 4 k) 2) 0 (+ (* 2 k) 1)))
+
+(: compose (-> IIII IIII IIII))
+(define (compose a b)
+  (match-define (list aq ar as_ at) a)
+  (match-define (list bq br bs bt) b)
+  (list (* aq bq)
+        (+ (* aq br) (* ar bt))
+        (+ (* as_ bq) (* at bs))
+        (+ (* as_ br) (* at bt))))
+
+(: extract (-> IIII Integer Integer))
+(define (extract z j)
+  (match-define (list q r s t) z)
+  (quotient (+ (* q j) r) (+ (* s j) t)))
+
+(: pi_digits (-> Integer (Listof Integer)))
+(define (pi_digits limit)
+   (define z : IIII (list 1 0 0 1))
+   (define x 1)
+   (define result : (Listof Integer) '())
+   (let loop ()
+     (when (<= x limit)
+       (define y (extract z 3))
+       (let loop ()
+         (when (not (= y (extract z 4)))
+           (set! z (compose z (gen_x x)))
+           (set! y (extract z 3))
+           (loop)))
+       (set! z (compose (list 10 (* -10 y) 0 1) z))
+       (set! x (+ x 1))
+       (set! result (append result (list y)))
+       (loop)))
+   result)
+
+(: calc_ndigits (-> Integer (Listof Integer)))
+(define (calc_ndigits n)
+  (pi_digits n))
+
+(define (main)
+  (calc_ndigits NDIGITS))
+
+(time (displayln (main)))
+;; goal = 
+
